2dd312cb8bf0b264b0a6ddab029752553bb177b3
krishkh <krishkhanna00@gmail.com>
2025-02-08 03:31:44 +0530

    bringing back cors

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify
-
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)  # Enables CORS for all routes
+CORS(app)  # Enables CORS for all routes
 
 
 @app.route("/")
